70f6b02310f60a053517903ece17c45b20580821
0ankit0 <ankitpaudel.1373@gmail.com>
2025-05-14 14:34:20 +0545

    Add CustomAuthStateProvider for user authentication
    Introduces a new `CustomAuthStateProvider` class that manages user authentication state in a Blazor application. It provides methods to mark users as authenticated or logged out and returns the current authentication state. Necessary namespaces for claims and asynchronous tasks are also imported.

---
+++ Recommendation_System.Web/Services/CustomAuthStateProvider.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+public class CustomAuthStateProvider : AuthenticationStateProvider
+{
+    private ClaimsPrincipal _currentUser = new ClaimsPrincipal(new ClaimsIdentity());
+
+    public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        => Task.FromResult(new AuthenticationState(_currentUser));
+
+    public void MarkUserAsAuthenticated(string email)
+    {
+        var identity = new ClaimsIdentity(new[]
+        {
+            new Claim(ClaimTypes.Name, email)
+        }, "apiauth_type");
+
+        _currentUser = new ClaimsPrincipal(identity);
+        NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+    }
+
+    public void MarkUserAsLoggedOut()
+    {
+        _currentUser = new ClaimsPrincipal(new ClaimsIdentity());
+        NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+    }
+}
